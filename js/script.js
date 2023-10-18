47152244d132f0b7dac9804745c18778ec840ead
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2023-10-18 10:59:01 +0200

    this is a test add

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
-const bntInfoElem = document.querySelector("bntInf");
-bntInfoElem.Styly.color = "red";
+const sentencize = (text) => {
+  return text.charAt(0).toUpperCase() + text.slice(1)+'.';
+};
+const btnInfoElem = document.querySelector(".btnInfo");
+
+
+btnInfoElem.addEventListener("click", () => {
+  const messageElem = document.querySelector(".message");
+  messageElem.innerHTML = sentencize('this is a test message');
+});

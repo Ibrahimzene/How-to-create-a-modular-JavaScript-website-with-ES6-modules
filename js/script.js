c57162bbdbd9b25d0e2eea803162d2063bb84a6b
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2023-10-18 11:36:09 +0200

    header export

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-import * as tools from './tools.js';
-
+import { sentencize, repleceAll }from './tools.js';
+import { Header } from '../components/headers.js';
 const sentencize = (text) => {
   return text.charAt(0).toUpperCase() + text.slice(1)+'.';
 };
 const btnInfoElem = document.querySelector(".btnInfo");
-
+const appElem = document.querySelector("#app");
 
 btnInfoElem.addEventListener("click", () => {
   const messageElem = document.querySelector(".message");
-  messageElem.innerHTML = tools.sentencize('this is a test message');
+  messageElem.innerHTML = sentencize(repleceAll)('this is a test message', 'and it is a test message');
+
 });
--- js/tools.js
+++ js/tools.js
@@ -1,3 +1,7 @@
 export const sentencize = (text) => {
     return text.charAt(0).toUpperCase() + text.slice(1)+'.';
 }
+
+export const repleceAll = (text, search, replacement) => {
+    return text.split(search).join (replacement);
+}
